15cf83979d01b5a94a5cb6ad1a941b2c61399759
MikeFilimonov <filimonov.michael@gmail.com>
2024-09-06 17:19:37 +0300

    roughgarden-course-practice

---
+++ fundamentals/main.go
@@ -0,0 +1,24 @@
+package main
+
+func main() {
+
+}
+
+/* Given an input of an unsorted array of n distinct numbers, where n is a power of 2.
+Give an algorithm that identifies the second-largest number in the array and that uses
+at most n + log2(N) -2. (Hint: Consider computing the largest number using a knockout
+tournament. )*/
+
+func secondLargest(input []int) int {
+
+	result := 0
+
+	return result
+
+}
+
+/* Implement Karatsuba's integer multiplication algorithm. To get the most out of this problem
+the code should invoke multiplication operator only on pairs of single-digit numbers.
+Get the product of the following two 64-digit dec numbers:
+3141592653589793238462643383279502884197169399375105820974944592 and
+2718281828459045235360287471352662497757247093699959574966967627*/
