4b921f553eeace9fa66ae0391c0ae0d88a15c1ea
MikeFilimonov <filimonov.michael@gmail.com>
2024-09-11 00:01:02 +0300

    faulty Maximum subarray

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
 	"MtCI-DSA/arrays"
 	"fmt"
+	"math"
+	"slices"
 	"strings"
 )
 
@@ -12,10 +14,164 @@ func main() {
 	// usingArrays()
 	// fmt.Println(reverseAString("Cleverics"))
 
-	left := []int{4, 5, 67, 776}
-	right := []int{14, 67, 68, 408}
+	// left := []int{4, 5, 67, 776}
+	// right := []int{14, 67, 68, 408}
 
-	fmt.Println(mergeSortedArrays(left, right))
+	// fmt.Println(mergeSortedArrays(left, right))
+
+	// nums := []int{3, 2, 4}
+
+	// fmt.Println(twoSum(nums, 6))
+
+	testData := [][]int{
+		{-2, 1, -3, 4, -1, 2, 1, -5, 4},
+		{1},
+		{5, 4, -1, 7, 8},
+		{-2, 1},
+		{-2, -1},
+	}
+
+	for _, input := range testData {
+		// fmt.Println(maxSubArrayByKadane(input))
+		fmt.Println(maxSubArrayDC(input, 0, len(input)-1))
+	}
+
+}
+
+// finds the continuous part of the array, consisting of the elements that give max value
+// when summed up, returns this very value
+// using divide and conquer technique
+// Returns sum of maximum sum subarray in nums[leftPoint ... rightPoint]
+func maxSubArrayDC(nums []int, leftPoint, rightPoint int) int {
+
+	// error handling: range like that can't exist
+	if leftPoint > rightPoint {
+		return math.MinInt
+	}
+
+	// the only element
+	if leftPoint == rightPoint {
+		return nums[leftPoint]
+	}
+
+	// Calculate the mid-point
+	midPoint := (leftPoint + rightPoint) / 2
+
+	// Calculate the maximum value:
+	// max value is in the leftPart of nums
+	// max value is in the rightPart of nums
+	// max value is in the intersection of right and left parts
+
+	return maxIntOfThree(
+		maxSubArrayDC(nums, leftPoint, midPoint-leftPoint),
+		maxSubArrayDC(nums, midPoint+1, rightPoint),
+		maxIntersectionArraySum(nums, leftPoint, midPoint, rightPoint),
+	)
+
+}
+
+//maxSubArrayDCHelpers{
+
+// find the maximum possible sum so subarray of m would be its part
+func maxIntersectionArraySum(nums []int, leftPoint int, middlePoint int, rightPoint int) int {
+
+	// Grab the elements to the left of the mid-element [leftPoint, middlePoint]
+	sum := 0
+	leftPartSum := math.MinInt
+
+	for i := middlePoint; i >= leftPoint; i-- {
+		sum += nums[i]
+		if sum > leftPartSum {
+			leftPartSum = sum
+		}
+	}
+
+	// Grab the elements to the right of the mid-element [middlePoint, rightPoint]
+	sum = 0
+	rightPartSum := math.MinInt
+	for i := middlePoint; i <= rightPoint; i++ {
+		sum += nums[i]
+		if sum > rightPartSum {
+			rightPartSum = sum
+		}
+	}
+
+	// Return the sum of elements on the left and on the right of the middle point,
+
+	return maxIntOfThree(leftPartSum+rightPartSum-nums[middlePoint], leftPartSum, rightPartSum)
+
+}
+
+func maxInt(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+func maxIntOfThree(a, b, c int) int {
+	subValue := maxInt(a, b)
+	return maxInt(subValue, c)
+}
+
+//}maxSubArrayDCHelpers
+
+// finds the continuous part of the array, consisting of the elements that give max value
+// when summed up, returns this very value
+// Kadane's algorithm: O(n^2)
+func maxSubArrayByKadane(nums []int) int {
+
+	result := nums[0]
+
+	// starting point of  the subarray
+	for index, _ := range nums {
+
+		currentSum := 0
+
+		// ending point of the subarray
+
+		for j := index; j < len(nums); j++ {
+			currentSum += nums[j]
+
+			// update result if currentSum overlaps it
+			if currentSum > result {
+				result = currentSum
+			}
+		}
+
+	}
+
+	return result
+
+}
+
+// returns the indices of the elements equal to the pair of summands
+func twoSum(nums []int, target int) []int {
+
+	var result []int
+
+	for i, v := range nums {
+
+		nextValue := target - v
+		fmt.Println(nums[i+1:])
+		secondOperandPosition := slices.Index(nums[i+1:], nextValue)
+
+		fmt.Println(v)
+		fmt.Println(nextValue)
+		fmt.Println(secondOperandPosition)
+
+		if secondOperandPosition == -1 {
+			continue
+		} else {
+			result = append(result, i)
+			result = append(result, i+secondOperandPosition+1)
+			break
+		}
+
+	}
+
+	return result
 
 }
 
