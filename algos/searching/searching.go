901d892c053599a8bed130c2b1bf49788fa63705
MikeFilimonov <filimonov.michael@gmail.com>
2024-10-13 21:53:07 +0300

    bfs and dfs theory quiz

---
+++ algos/searching/searching.go
@@ -0,0 +1,32 @@
+package searching
+
+func theory() {
+
+	/* BFS vs DFS
+
+	   //If you know a solution is not far from the root of the tree:
+	   BFS
+
+
+	   //If the tree is very deep and solutions are rare:
+	   BFS as DFS will take took long
+
+
+	   //If the tree is very wide:
+		DFS as BFS will take too much space
+
+
+	   //If solutions are frequent but located deep in the tree:
+	   DFS
+
+
+	   //Determining whether a path exists between two nodes:
+	   DFS
+
+
+	   //Finding the shortest path:
+	   BFS
+
+	*/
+
+}
